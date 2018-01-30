84eab430ec95b67c31a17997c09f9482344c3501
ligulfzhou <ligulfzhou53@gmail.com>
2018-01-30 22:30:30 +0800

    change proto3 to proto2

--- proto/paymentrequest_pb2.py
+++ proto/paymentrequest_pb2.py
@@ -18,8 +18,8 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='paymentrequest.proto',
   package='payments',
-  syntax='proto3',
-  serialized_pb=_b('\n\x14paymentrequest.proto\x12\x08payments\"(\n\x06Output\x12\x0e\n\x06\x61mount\x18\x01 \x01(\x04\x12\x0e\n\x06script\x18\x02 \x01(\x0c\"\x9d\x01\n\x0ePaymentDetails\x12\x0f\n\x07network\x18\x01 \x01(\t\x12!\n\x07outputs\x18\x02 \x03(\x0b\x32\x10.payments.Output\x12\x0c\n\x04time\x18\x03 \x01(\x04\x12\x0f\n\x07\x65xpires\x18\x04 \x01(\x04\x12\x0c\n\x04memo\x18\x05 \x01(\t\x12\x13\n\x0bpayment_url\x18\x06 \x01(\t\x12\x15\n\rmerchant_data\x18\x07 \x01(\x0c\"\x8c\x01\n\x0ePaymentRequest\x12\x1f\n\x17payment_details_version\x18\x01 \x01(\r\x12\x10\n\x08pki_type\x18\x02 \x01(\t\x12\x10\n\x08pki_data\x18\x03 \x01(\x0c\x12\"\n\x1aserialized_payment_details\x18\x04 \x01(\x0c\x12\x11\n\tsignature\x18\x05 \x01(\x0c\"\'\n\x10X509Certificates\x12\x13\n\x0b\x63\x65rtificate\x18\x01 \x03(\x0c\"i\n\x07Payment\x12\x15\n\rmerchant_data\x18\x01 \x01(\x0c\x12\x14\n\x0ctransactions\x18\x02 \x03(\x0c\x12#\n\trefund_to\x18\x03 \x03(\x0b\x32\x10.payments.Output\x12\x0c\n\x04memo\x18\x04 \x01(\t\">\n\nPaymentACK\x12\"\n\x07payment\x18\x01 \x01(\x0b\x32\x11.payments.Payment\x12\x0c\n\x04memo\x18\x02 \x01(\tb\x06proto3')
+  syntax='proto2',
+  serialized_pb=_b('\n\x14paymentrequest.proto\x12\x08payments\"+\n\x06Output\x12\x11\n\x06\x61mount\x18\x01 \x01(\x04:\x01\x30\x12\x0e\n\x06script\x18\x02 \x02(\x0c\"\xa3\x01\n\x0ePaymentDetails\x12\x15\n\x07network\x18\x01 \x01(\t:\x04main\x12!\n\x07outputs\x18\x02 \x03(\x0b\x32\x10.payments.Output\x12\x0c\n\x04time\x18\x03 \x02(\x04\x12\x0f\n\x07\x65xpires\x18\x04 \x01(\x04\x12\x0c\n\x04memo\x18\x05 \x01(\t\x12\x13\n\x0bpayment_url\x18\x06 \x01(\t\x12\x15\n\rmerchant_data\x18\x07 \x01(\x0c\"\x95\x01\n\x0ePaymentRequest\x12\"\n\x17payment_details_version\x18\x01 \x01(\r:\x01\x31\x12\x16\n\x08pki_type\x18\x02 \x01(\t:\x04none\x12\x10\n\x08pki_data\x18\x03 \x01(\x0c\x12\"\n\x1aserialized_payment_details\x18\x04 \x02(\x0c\x12\x11\n\tsignature\x18\x05 \x01(\x0c\"\'\n\x10X509Certificates\x12\x13\n\x0b\x63\x65rtificate\x18\x01 \x03(\x0c\"i\n\x07Payment\x12\x15\n\rmerchant_data\x18\x01 \x01(\x0c\x12\x14\n\x0ctransactions\x18\x02 \x03(\x0c\x12#\n\trefund_to\x18\x03 \x03(\x0b\x32\x10.payments.Output\x12\x0c\n\x04memo\x18\x04 \x01(\t\">\n\nPaymentACK\x12\"\n\x07payment\x18\x01 \x02(\x0b\x32\x11.payments.Payment\x12\x0c\n\x04memo\x18\x02 \x01(\tB(\n\x1eorg.bitcoin.protocols.paymentsB\x06Protos')
 )
 
 
@@ -35,13 +35,13 @@ _OUTPUT = _descriptor.Descriptor(
     _descriptor.FieldDescriptor(
       name='amount', full_name='payments.Output.amount', index=0,
       number=1, type=4, cpp_type=4, label=1,
-      has_default_value=False, default_value=0,
+      has_default_value=True, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='script', full_name='payments.Output.script', index=1,
-      number=2, type=12, cpp_type=9, label=1,
+      number=2, type=12, cpp_type=9, label=2,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -54,12 +54,12 @@ _OUTPUT = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto3',
+  syntax='proto2',
   extension_ranges=[],
   oneofs=[
   ],
   serialized_start=34,
-  serialized_end=74,
+  serialized_end=77,
 )
 
 
@@ -73,7 +73,7 @@ _PAYMENTDETAILS = _descriptor.Descriptor(
     _descriptor.FieldDescriptor(
       name='network', full_name='payments.PaymentDetails.network', index=0,
       number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
+      has_default_value=True, default_value=_b("main").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
@@ -86,7 +86,7 @@ _PAYMENTDETAILS = _descriptor.Descriptor(
       options=None),
     _descriptor.FieldDescriptor(
       name='time', full_name='payments.PaymentDetails.time', index=2,
-      number=3, type=4, cpp_type=4, label=1,
+      number=3, type=4, cpp_type=4, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -127,12 +127,12 @@ _PAYMENTDETAILS = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto3',
+  syntax='proto2',
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=77,
-  serialized_end=234,
+  serialized_start=80,
+  serialized_end=243,
 )
 
 
@@ -146,14 +146,14 @@ _PAYMENTREQUEST = _descriptor.Descriptor(
     _descriptor.FieldDescriptor(
       name='payment_details_version', full_name='payments.PaymentRequest.payment_details_version', index=0,
       number=1, type=13, cpp_type=3, label=1,
-      has_default_value=False, default_value=0,
+      has_default_value=True, default_value=1,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
       name='pki_type', full_name='payments.PaymentRequest.pki_type', index=1,
       number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
+      has_default_value=True, default_value=_b("none").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
@@ -166,7 +166,7 @@ _PAYMENTREQUEST = _descriptor.Descriptor(
       options=None),
     _descriptor.FieldDescriptor(
       name='serialized_payment_details', full_name='payments.PaymentRequest.serialized_payment_details', index=3,
-      number=4, type=12, cpp_type=9, label=1,
+      number=4, type=12, cpp_type=9, label=2,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -186,12 +186,12 @@ _PAYMENTREQUEST = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto3',
+  syntax='proto2',
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=237,
-  serialized_end=377,
+  serialized_start=246,
+  serialized_end=395,
 )
 
 
@@ -217,12 +217,12 @@ _X509CERTIFICATES = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto3',
+  syntax='proto2',
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=379,
-  serialized_end=418,
+  serialized_start=397,
+  serialized_end=436,
 )
 
 
@@ -269,12 +269,12 @@ _PAYMENT = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto3',
+  syntax='proto2',
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=420,
-  serialized_end=525,
+  serialized_start=438,
+  serialized_end=543,
 )
 
 
@@ -287,7 +287,7 @@ _PAYMENTACK = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='payment', full_name='payments.PaymentACK.payment', index=0,
-      number=1, type=11, cpp_type=10, label=1,
+      number=1, type=11, cpp_type=10, label=2,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -307,12 +307,12 @@ _PAYMENTACK = _descriptor.Descriptor(
   ],
   options=None,
   is_extendable=False,
-  syntax='proto3',
+  syntax='proto2',
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=527,
-  serialized_end=589,
+  serialized_start=545,
+  serialized_end=607,
 )
 
 _PAYMENTDETAILS.fields_by_name['outputs'].message_type = _OUTPUT
@@ -369,4 +369,6 @@ PaymentACK = _reflection.GeneratedProtocolMessageType('PaymentACK', (_message.Me
 _sym_db.RegisterMessage(PaymentACK)
 
 
+DESCRIPTOR.has_options = True
+DESCRIPTOR._options = _descriptor._ParseOptions(descriptor_pb2.FileOptions(), _b('\n\036org.bitcoin.protocols.paymentsB\006Protos'))
 # @@protoc_insertion_point(module_scope)
